17c4084b1ba4dfd61fcbb73bf5df1a6422ec3d48
MohiuddinCo22 <161435524+MohiuddinCo22@users.noreply.github.com>
2024-02-27 19:09:25 +0600

    Update script.js

--- script.js
+++ script.js
@@ -156,3 +156,9 @@ function scrollHeader(){
     if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+var loader = document.getElementById("center");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})
